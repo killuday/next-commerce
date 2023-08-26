86645f3d5db16ecd411bce37d7601c23c14ec077
Uday Bhanu <bhanusharm122@gmail.com>
2023-08-26 19:32:47 +0530

    styles update

--- pages/hoodies.js
+++ pages/hoodies.js
@@ -1,124 +1,109 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+
+const hoodieData = [
+  {
+    id: 1,
+    name: "Classic Hoodie",
+    price: 29.99,
+    sizes: ["S", "M", "L", "XL"],
+    color: "Black",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/57/11/5711b9dcaebb68cfd81bd965cb38ff1a0d921e52.jpg],origin[dam],category[men_hoodiessweatshirts_hoodies],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+  },
+  {
+    id: 2,
+    name: "Sporty Hoodie",
+    price: 34.99,
+    sizes: ["S", "M", "L", "XL"],
+    color: "Black/White",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/57/11/5711b9dcaebb68cfd81bd965cb38ff1a0d921e52.jpg],origin[dam],category[men_hoodiessweatshirts_hoodies],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+  },
+  {
+    id: 3,
+    name: "Cozy Hoodie",
+    price: 39.99,
+    sizes: ["XS", "S", "M", "L", "XL"],
+    color: "Pink",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/57/11/5711b9dcaebb68cfd81bd965cb38ff1a0d921e52.jpg],origin[dam],category[men_hoodiessweatshirts_hoodies],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+  },
+  {
+    id: 4,
+    name: "Urban Hoodie",
+    price: 45.99,
+    sizes: ["S", "M", "L", "XL"],
+    color: "Grey/Black",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/57/11/5711b9dcaebb68cfd81bd965cb38ff1a0d921e52.jpg],origin[dam],category[men_hoodiessweatshirts_hoodies],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+  },
+  {
+    id: 5,
+    name: "Vintage Hoodie",
+    price: 32.99,
+    sizes: ["S", "M", "L"],
+    color: "Faded Black",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/57/11/5711b9dcaebb68cfd81bd965cb38ff1a0d921e52.jpg],origin[dam],category[men_hoodiessweatshirts_hoodies],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+  },
+  {
+    id: 6,
+    name: "Graphic Print Hoodie",
+    price: 27.99,
+    sizes: ["S", "M", "L", "XL"],
+    color: "White/Black",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/57/11/5711b9dcaebb68cfd81bd965cb38ff1a0d921e52.jpg],origin[dam],category[men_hoodiessweatshirts_hoodies],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+  },
+  {
+    id: 7,
+    name: "Athletic Hoodie",
+    price: 36.99,
+    sizes: ["M", "L", "XL"],
+    color: "Blue",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/57/11/5711b9dcaebb68cfd81bd965cb38ff1a0d921e52.jpg],origin[dam],category[men_hoodiessweatshirts_hoodies],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+  },
+  {
+    id: 8,
+    name: "Knitted Hoodie",
+    price: 49.99,
+    sizes: ["S", "M", "L"],
+    color: "Cream",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/57/11/5711b9dcaebb68cfd81bd965cb38ff1a0d921e52.jpg],origin[dam],category[men_hoodiessweatshirts_hoodies],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+  },
+  // Add more hoodie products here...
+];
+
 
 const Hoodies = () => {
   return (
-    <div>
+    <>
       <section className="text-gray-600 body-font">
-  <div className="container px-5 py-24 mx-auto">
-    <div className="flex flex-wrap -m-4">
-    <Link legacyBehavior  href={'/product/relaxed-fit'}>
-     <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto md:mx-0 md:h-[40vh] h-[36vh] block" src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F8e%2F6f%2F8e6f2c8102615545af8bc821190b3e0a9dc8221a.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]" />
-        </a>
-        <div className="mt-4 text-center md:text-left ">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Relaxed Fit</h2>
-          <p className="mt-1">₹1299</p>
-          <p className="mt-1">XS, S, M ,L ,XL, XXL</p>
-        </div>
-      </div>
-      </Link>
-      
-    
-      <Link legacyBehavior  href={'/product/relaxed-fit'}>
-        <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto md:mx-0 md:h-[40vh] h-[36vh] block" src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F8e%2F6f%2F8e6f2c8102615545af8bc821190b3e0a9dc8221a.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]" />
-        </a>
-        <div className="mt-4 text-center md:text-left ">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Relaxed Fit</h2>
-          <p className="mt-1">₹1299</p>
-          <p className="mt-1">XS, S, M ,L ,XL, XXL</p>
-        </div>
-      </div>
-      </Link>
-      <Link legacyBehavior  href={'/product/relaxed-fit'}>
-     <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto md:mx-0 md:h-[40vh] h-[36vh] block" src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F8e%2F6f%2F8e6f2c8102615545af8bc821190b3e0a9dc8221a.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]" />
-        </a>
-        <div className="mt-4 text-center md:text-left ">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Relaxed Fit</h2>
-          <p className="mt-1">₹1299</p>
-          <p className="mt-1">XS, S, M ,L ,XL, XXL</p>
-        </div>
-      </div>
-      </Link>
-      <Link legacyBehavior  href={'/product/relaxed-fit'}>
-    <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto md:mx-0 md:h-[40vh] h-[36vh] block" src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F8e%2F6f%2F8e6f2c8102615545af8bc821190b3e0a9dc8221a.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]" />
-        </a>
-        <div className="mt-4 text-center md:text-left ">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Relaxed Fit</h2>
-          <p className="mt-1">₹1299</p>
-          <p className="mt-1">XS, S, M ,L ,XL, XXL</p>
-        </div>
-      </div>
-      </Link>
-      <Link legacyBehavior  href={'/product/relaxed-fit'}>
-     <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto md:mx-0 md:h-[40vh] h-[36vh] block" src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F8e%2F6f%2F8e6f2c8102615545af8bc821190b3e0a9dc8221a.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]" />
-        </a>
-        <div className="mt-4 text-center md:text-left ">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Relaxed Fit</h2>
-          <p className="mt-1">₹1299</p>
-          <p className="mt-1">XS, S, M ,L ,XL, XXL</p>
-        </div>
-      </div>
-      </Link>
-      <Link legacyBehavior  href={'/product/relaxed-fit'}>
-     <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto md:mx-0 md:h-[40vh] h-[36vh] block" src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F8e%2F6f%2F8e6f2c8102615545af8bc821190b3e0a9dc8221a.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]" />
-        </a>
-        <div className="mt-4 text-center md:text-left ">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Relaxed Fit</h2>
-          <p className="mt-1">₹1299</p>
-          <p className="mt-1">XS, S, M ,L ,XL, XXL</p>
-        </div>
-      </div>
-      </Link>
-      <Link legacyBehavior  href={'/product/relaxed-fit'}>
-    <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto md:mx-0 md:h-[40vh] h-[36vh] block" src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F8e%2F6f%2F8e6f2c8102615545af8bc821190b3e0a9dc8221a.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]" />
-        </a>
-        <div className="mt-4 text-center md:text-left ">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Relaxed Fit</h2>
-          <p className="mt-1">₹1299</p>
-          <p className="mt-1">XS, S, M ,L ,XL, XXL</p>
-        </div>
-      </div>
-      </Link>
-    <Link legacyBehavior  href={'/product/relaxed-fit'}>
-     <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto md:mx-0 md:h-[40vh] h-[36vh] block" src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F8e%2F6f%2F8e6f2c8102615545af8bc821190b3e0a9dc8221a.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]" />
-        </a>
-        <div className="mt-4 text-center md:text-left ">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Relaxed Fit</h2>
-          <p className="mt-1">₹1299</p>
-          <p className="mt-1">XS, S, M ,L ,XL, XXL</p>
-        </div>
-      </div>
-      </Link>
-      
-    </div>
-  </div>
-</section>
-    </div>
-  )
-}
+        <div className="container  px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <Link  href={"/product/relaxed-fit"}>
+              <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 m-5 lg:grid-cols-4 xl:gap-x-8 ">
 
-export default Hoodies
+              {hoodieData.map((hoodie) => (
+                <>
+                  <div key={hoodie.id} className=" border shadow-xl rounded-xl ">
+                    <div className="">
+                      <img src={hoodie.imageUrl} className="w-full hover:opacity-75 h-full object-contain rounded-t-xl " alt="" /> 
+                    </div>
+                    <div className="flex justify-between p-5">
+                      <div>
+                      <h2>{hoodie.name}</h2>
+                      
+                      <p className="truncate">{hoodie.colors}</p>
+                      <p>{hoodie.sizes}</p>
+                      </div>
+                      <p>₹{hoodie.price}</p>
+                    </div>
+                  </div>
+                </>
+              ))}
+              </div>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hoodies;
